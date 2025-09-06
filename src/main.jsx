10212d92c03d8d62983cbeeb59b193f0c8728e98
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2025-09-06 11:13:20 +0530

    Implement routing with react-router-dom and update package dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom"
 import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    errorElement : <div>Not Found</div>,
+    element : <App />,
+    children : [
+      {
+        path: "/",
+        element : <div>Home</div>
+      },
+      {
+        path : "/issues",
+        element : <div>Issues</div>
+      },
+      {
+        path : "/allIssues",
+        element : <div>All Issues</div>
+      },
+      {
+        path : "/allIssues/:id",
+        element : <div>id</div>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 )
